01c0e93cf45e6ddb7f05875c313fc57aa501cd44
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-04-13 23:35:01 +0700

    Udh parse rss

---
+++ GetNews/GetNews/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml;
+using System.ServiceModel.Syndication;
+using System.Linq;
+
+namespace GetNews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] urls = System.IO.File.ReadAllLines("NewsWebsites.txt");
+
+            foreach (string url in urls)
+            {
+                Console.WriteLine(url);
+                Console.WriteLine();
+
+                XmlReader xml_reader = XmlReader.Create(url);
+                SyndicationFeed feed = SyndicationFeed.Load(xml_reader);
+
+                xml_reader.Close();
+                int cnt = 0;
+
+                foreach(SyndicationItem item in feed.Items)
+                {
+                    cnt++;
+
+                    string subject = item.Title.Text;
+                    string published = item.PublishDate.ToString();
+                    string link = item.Links.First().Uri.ToString();
+
+                    Console.WriteLine("Berita #" + cnt + ":");
+                    Console.WriteLine("Subject : " + subject);
+                    Console.WriteLine("Published : " + published);
+                    Console.WriteLine("Link : " + link);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
