0727a0f6c475c86dbaef22b457ed3ff52071fc9d
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-04-18 11:26:30 +0700

    Front end integrated with project

---
+++ RainyDay/RainyDay/wwwroot/js/time.js
@@ -0,0 +1,31 @@
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var timer = setInterval(Timer, 1000);
+
+function Timer() {
+    var d = new Date();
+    var s = "";
+
+    s += days[d.getDay()];
+    s += ", " + d.getDate();
+    s += " " + months[d.getMonth()];
+    s += " " + d.getFullYear();
+
+    var val = d.getHours();
+    if (val < 10) s += "\xa0\xa00"; // character \xa0 = non breaking space
+    else s += "\xa0\xa0";
+    s += val;
+
+    var val = d.getMinutes();
+    if (val < 10) s += ":0";
+    else s += ":";
+    s += val;
+
+    var val = d.getSeconds();
+    if (val < 10) s += ":0";
+    else s += ":";
+    s += val;
+
+    document.getElementById("time").innerHTML = s;
+}
