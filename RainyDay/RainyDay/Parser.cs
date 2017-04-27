054e0bc4faa8c67e73f7600a1a406ea4226295a6
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-04-27 22:22:06 +0700

    Berita udh dpet, newline belom ilang

--- RainyDay/RainyDay/Parser.cs
+++ RainyDay/RainyDay/Parser.cs
@@ -12,8 +12,9 @@ namespace RainyDay
 {
     public class Parser
     {
-        string[] url_list;
-        List<News> news_list; 
+        string[] url_list; // list of RSS URL
+        List<News> raw_news_list; // initial news list
+        List<News> news_list; // final news list
 
         public void ReadWebsitesFromFile(string filename)
         {
@@ -22,65 +23,103 @@ namespace RainyDay
 
         public void ParseRSS()
         {
-            news_list = new List<News>(); // Create new List of News
+            raw_news_list = new List<News>(); // Create new List of News
 
             XmlReader xml_reader;
             XmlDocument xml_doc = new XmlDocument();
+            int max = 20; // maximum number of news
 
-            foreach (string url in url_list) // for each url
+            foreach (string url in url_list) // for each URL
             {
-                xml_reader = XmlReader.Create(url); // read xml from specified url
-                xml_doc.Load(xml_reader); // create xml document
+                try
+                {
+                    xml_reader = XmlReader.Create(url); // read xml from specified URL
+                }
+                catch(WebException w)
+                {
+                    continue; // skip URL
+                }
 
+                xml_doc.Load(xml_reader); // create xml document
                 XmlNodeList nodes = xml_doc.SelectNodes("//rss/channel/item"); // get all <item> as node list using XPath
+
+                int counter = 0;
 
                 foreach (XmlNode node in nodes) // for each <item>
                 {
-                    XmlNode title = node.SelectSingleNode("title"); // get news title using XPath
-                    XmlNode pubDate = node.SelectSingleNode("pubDate"); // get news date using XPath
-                    XmlNode link = node.SelectSingleNode("link"); // get news link using XPath
+                    counter++;
 
-                    string news_title = title.InnerText; // get content from title node
-                    string news_date = pubDate.InnerText; // get content from pubDate node
-                    string news_link = link.InnerText; // get content from link node
+                    if (counter <= 20)
+                    {
+                        XmlNode title = node.SelectSingleNode("title"); // get news title using XPath
+                        XmlNode pubDate = node.SelectSingleNode("pubDate"); // get news date using XPath
+                        XmlNode link = node.SelectSingleNode("link"); // get news link using XPath
 
-                    News news = new News(news_title, news_date, news_link, ""); // create News object
-                    news_list.Add(news); // add to news list
+                        string news_title = title.InnerText; // get content from title node
+                        string news_date = pubDate.InnerText; // get content from pubDate node
+                        string news_link = link.InnerText; // get content from link node
+
+                        News news = new News(news_title, news_date, news_link, ""); // create News object
+                        raw_news_list.Add(news); // add to news list
+                    }
+                    else break;
                 }
             }
         }
 
         public void ParseHTML()
         {
+            news_list = new List<News>();
+
             // Website RSS URL
-            string viva = "rss.viva.co.id";
-            string antara = "www.antaranews.com/rss/";
-            string tempo = "www.tempo.co/rss/";
-            string detik = "rss.detik.com";
+            string viva = "viva\\.co\\.id";
+            string antara = "antaranews\\.com";
+            string tempo = "www\\.tempo\\.co";
+            string detik = "rss\\.detik\\.com";
 
             // Web Client to download HTML
             WebClient webclient = new WebClient();
             webclient.Encoding = System.Text.Encoding.UTF8; // set encoding to UTF-8
+            string page_html;
             
-            foreach (News _news in news_list)
+            foreach (News _news in raw_news_list)
             {
                 string news_link = _news.link;
-                string page_html = webclient.DownloadString(news_link); // download HTML
-                
+
+                try
+                {
+                    page_html = webclient.DownloadString(news_link); // download HTML
+                }
+                catch(WebException w)
+                {
+                    continue; // skip item
+                }
+
                 // Determine website from URL
                 if (Regex.IsMatch(news_link, viva)) // if website is viva
                 {
-                    string viva_news = "<\\s+span\\s+itemprop\\s+=\\s+\"description\"\\s+><\\s+p\\s+><\\s+strong\\s+>\\s+VIVA.co.id\\s+</\\s+strong\\s+>\\s+–\\s+(.+)<\\s+span>";
-                    Match news_match = Regex.Match(page_html, viva_news, RegexOptions.Singleline); // get news
+                    string raw_news;
+                    Match news_match;
 
-                    string raw_news = news_match.Groups[1].Value; // get 1st subexpression
+                    // get news
+                    string viva_news_regex1 = "<span itemprop=\"description\"><p><strong>VIVA.co.id</strong>\\s*–\\s*(.+)";
+                    news_match = Regex.Match(page_html, viva_news_regex1, RegexOptions.Singleline); 
+                    raw_news = news_match.Groups[1].Value;
+
+                    string viva_news_regex2 = "^(.+)</p>\\s*</span>";
+                    news_match = Regex.Match(raw_news, viva_news_regex2, RegexOptions.Singleline);
+                    raw_news = news_match.Groups[1].Value;
+
+                    raw_news = Regex.Replace(raw_news, "<.+>", "", RegexOptions.Singleline); // remove remaining tag
+                    raw_news = Regex.Replace(raw_news, "\n", " ", RegexOptions.Singleline); //replace all newline with spaces.
+
                     string formatted_news = Regex.Replace(raw_news, "\\s+", " "); // replace all consecutive whitespaces with a single space
 
                     _news.content = formatted_news;
-
-                    System.Diagnostics.Debug.WriteLine(formatted_news);
+                    news_list.Add(_news); // add to final news list
 
                     System.IO.File.AppendAllText("tes.txt",formatted_news);
+                    System.IO.File.AppendAllText("tes.txt", "\n");
                 }
                 else if (Regex.IsMatch(news_link, antara)) // if website is antara
                 {
